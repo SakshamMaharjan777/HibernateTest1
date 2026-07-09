46a9d7908e97a4cbd1674ee650b3605b21ea7f67
saksh <sakshammaharjan9@gmail.com>
2026-07-09 07:53:29 +0545

    jul9 commit

--- src/main/java/io/virinchi/App.java
+++ src/main/java/io/virinchi/App.java
@@ -1,5 +1,10 @@
 package io.virinchi;
 
+import io.virinchi.model.UserClass;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -18,6 +23,26 @@ public class App
     {
 
 
-        System.out.println( "Hello World!" );
+        //session create garera database ma hibernate ko through table ni banaunu paryo data ni pathaunu paryo
+        //session banauxa session factory le
+
+        SessionFactory sf= new Configuration().configure().buildSessionFactory();
+        //session factory bata configurational settings taneko
+        Session session=sf.openSession();
+        //ek session ma multiple hibernate transaction huna sakxa tyo open gareko
+
+        UserClass uc = new UserClass();
+        //model ko class jun aba table bancha tesko column ko data rakheko
+        uc.setName("jack");
+
+
+        session.persist(uc);
+        //session.persist bhneko yo session ma hamro moddel ko class ko table bannu paryo
+        //or baneko table ma data basnu paryo
+        //persist le data rakhna help garxa(save bhneko jasto ho)
+        session.beginTransaction().commit();
+        //commit le kaam suru garcha
+        session.close();
+
     }
 }
